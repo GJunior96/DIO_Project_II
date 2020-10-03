7dffee983cda2805d9f4593e6590a2f0cdcf469f
Gilberto Júnio Almada <gilbertojunioralmada@gmail.com>
2020-10-03 12:52:05 -0300

    DeleteProduct

---
+++ DeleteProduct/index.js
@@ -0,0 +1,18 @@
+const { objectID} = require('mongodb');
+const createMongoClient = require('../shared/mongoClient');
+
+module.exports = async function (context, req) {
+    const { id } = req.params;
+    const product = req.body;
+    
+    const { client: MongoClient, closeConnectionFn } = await createMongoClient();
+    const Products = MongoClient.collection('products');
+    const res = await  Products.findOneAndDelete({ _id: ObjectID(id) });
+    
+    
+    closeConnectionFn();
+    contex.res = {
+        status: 200,
+        body: res
+    }
+}
